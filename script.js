9a49a5f80169d756a8994c97a69ab39cbb4cf097
Christopher Mendoza <tiptop1112@gmail.com>
2024-09-28 12:07:23 -0700

    Selected DOM elements for the form inputs and submit button

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ function Book (author, title, pages) {
     this.pages = pages;
 }
 
+// select the DOM inputs and submit button within the form 
+const title = document.querySelector('input[name="title"]');
+const author = document.querySelector('input[name="author"]');
+const pages = document.querySelector('input[name="pages_total"]');
+const read = document.getElementsByName('readOrNot');
+const submit = document.querySelector('#submit-button');
 //a function that takes users input and stores them as new book objects into the myLibrary array
