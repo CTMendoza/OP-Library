bc29cc06a091063f610f580c50afa7acf1b39762
Christopher Mendoza <tiptop1112@gmail.com>
2024-09-30 09:58:17 -0700

    Added eventlistener function to submit button Added event listener function to submit button that creates an object of the input values then pushes the object into a myLibrary array. Next step is to take each object and create a 'card' element to visualize book info on the app.

--- script.js
+++ script.js
@@ -2,16 +2,46 @@
 const myLibrary = [];
 
 //users book objects are created through Books constructor
-function Book (author, title, pages) {
+function Book (author, title, pages, readStatus) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.readStatus = readStatus
 }
 
 // select the DOM inputs and submit button within the form 
+const form = document.querySelector('#book-form');
 const title = document.querySelector('input[name="title"]');
 const author = document.querySelector('input[name="author"]');
-const pages = document.querySelector('input[name="pages_total"]');
+const pages = document.querySelector('input[name="page_total"]');
 const read = document.getElementsByName('readOrNot');
 const submit = document.querySelector('#submit-button');
+
 //a function that takes users input and stores them as new book objects into the myLibrary array
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    // Determine the value of the selected radio button
+    let readStatus;
+    for (const input of read) {
+        if (input.checked) {
+            readStatus = input.value;
+            break;
+        }
+    }
+    //store input values in new Book object
+    const newBook = new Book(title.value, author.value, pages.value, readStatus);
+
+    //store neBook object in myLibrary array
+    myLibrary.push(newBook);
+
+    // display myLibrary array in console
+    console.log(myLibrary);
+
+    //clear form inputs
+    form.reset(); 
+})
+
+
+
+
